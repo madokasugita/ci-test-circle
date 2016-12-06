476c1e8426a66e10bbe205e7bb2267fc5e0a20c0
madokasugita <madoka_sugita@cbase.co.jp>
2016-12-06 10:47:02 +0900

    ci-message-test

--- check_syntax.sh
+++ check_syntax.sh
@@ -17,13 +17,10 @@ if [ -n "$CI_PULL_REQUEST" ]; then
     detected=$(echo "$WARN" | grep "error")
 
     if [ -n "$detected" ]; then
-	git diff --name-only origin/develop \
-        | grep -e '.php$' \
-	| xargs vendor/bin/phpcs -n --standard=PSR2 --report=checkstyle \
-	| bundle exec checkstyle_filter-git diff origin/develop \
-        | bundle exec saddler report \
-        --require saddler/reporter/github \
-        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+	echo "$WARN" \
+            | bundle exec saddler report \
+            --require saddler/reporter/github \
+            --reporter Saddler::Reporter::Github::PullRequestReviewComment
 	exit 1
     fi
 fi
