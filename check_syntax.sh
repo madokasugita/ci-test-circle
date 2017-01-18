f6e20ec2a94b6690342ebad30def4e3ed349c5c9
yamakawa <yamakawa@cbase.co.jp>
2017-01-18 18:12:26 +0900

    csの処理を追加！！！

--- check_syntax.sh
+++ check_syntax.sh
@@ -12,8 +12,16 @@ fi
 if [ -n "$CI_PULL_REQUEST" ]; then
     git diff --name-only origin/develop \
         | grep -e '.php$' \
-        | xargs vendor/bin/phpcs -n --standard=PSR2 --report=checkstyle \
+        | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle \
+        | bundle exec checkstyle_filter-git diff origin/develop \
+        | bundle exec saddler report \
+        --require saddler/reporter/github \
+        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+        
+    git diff --name-only origin/develop \
+        | grep -e '.php$' \
         | xargs -I{} vendor/bin/phpmd {} text rules/phpmd_rules.xml \
+        | uniq -c -d \
         | bundle exec checkstyle_filter-git diff origin/develop \
         | bundle exec saddler report \
         --require saddler/reporter/github \
