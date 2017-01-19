56236f03495683a917808a0efae54112b2baac04
yamakawa00 <leokun0210@gmail.com>
2017-01-19 11:10:50 +0900

    コメント追加

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Start"
+echo "START SHELL: $0"
 LIST=`git diff --name-only origin/develop | grep -e '.php$'`
 
 if [ -z "$LIST" ]; then
@@ -11,20 +11,27 @@ fi
 
 if [ -n "$CI_PULL_REQUEST" ]; then
     cp -R vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility
+    echo "START PROCESS: phpcs"
     git diff --name-only origin/develop \
         | grep -e '.php$' \
-        | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle \
-        | bundle exec checkstyle_filter-git diff origin/develop \
-        | bundle exec saddler report \
-        --require saddler/reporter/github \
-        --reporter Saddler::Reporter::Github::PullRequestReviewComment
-        
+        | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle 
+        # | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle \
+        # | bundle exec checkstyle_filter-git diff origin/develop \
+        # | bundle exec saddler report 
+        # --require saddler/reporter/github \
+        # --reporter Saddler::Reporter::Github::PullRequestReviewComment
+    echo "END PROCESS: phpcs"
+    
+    echo "START PROCESS: phpmd"
     git diff --name-only origin/develop \
         | grep -e '.php$' \
         | xargs -I{} vendor/bin/phpmd {} text rules/phpmd_rules.xml \
-        | uniq -c -d \
-        | bundle exec checkstyle_filter-git diff origin/develop \
-        | bundle exec saddler report \
-        --require saddler/reporter/github \
-        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+        | uniq -c -d 
+        # | uniq -c -d \
+        # | bundle exec checkstyle_filter-git diff origin/develop \
+        # | bundle exec saddler report \
+        # --require saddler/reporter/github \
+        # --reporter Saddler::Reporter::Github::PullRequestReviewComment
+    echo "END PROCESS: phpmd"
 fi
+echo "END SHELL: $0"
