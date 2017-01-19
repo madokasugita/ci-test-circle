86f19c5447c10ea453aa9d28eb6867fcd5749918
yamakawa00 <leokun0210@gmail.com>
2017-01-19 15:45:33 +0900

    update xml

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,36 +1,85 @@
 #!/bin/bash
 
-echo "START SHELL: $0"
+set -eu
+
+echo "********************"
+echo "* start $0"
+echo "********************"
+echo "* repository :$CIRCLE_PROJECT_REPONAME"
+echo "* branch     :$CIRCLE_BRANCH"
+echo "* github url :$CIRCLE_COMPARE_URL"
+echo "* user       :$CIRCLE_PROJECT_USENAME"
+echo "********************"
+
 LIST=`git diff --name-only origin/develop | grep -e '.php$'`
 
 if [ -z "$LIST" ]; then
+    echo "********************"
     echo "PHP file not changed."
+    echo "********************"
     exit 0
 fi
 
+echo "********************"
+echo "* install gems"
+echo "********************"
+gem install --no-document checkstyle_filter-git saddler saddler-reporter-github pmd_translate_checkstyle_format
 
-if [ -n "$CI_PULL_REQUEST" ]; then
-    cp -R vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility
-    echo "START PROCESS: phpcs"
-    git diff --name-only origin/develop \
-        | grep -e '.php$' \
-        | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle \
-        | bundle exec checkstyle_filter-git diff origin/develop \
-        | bundle exec saddler report \
-        --require saddler/reporter/github \
-        --reporter Saddler::Reporter::Github::PullRequestReviewComment
-    echo "END PROCESS: phpcs"
+echo "********************"
+echo "* copy PHPCompatibility"
+echo "********************"
+cp -R vendor/wimg/php-compatibility vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/PHPCompatibility
+
+set +e
+echo "********************"
+echo "* exec check"
+echo "********************"
+git diff --name-only origin/develop \
+    | grep -e '.php$' \
+    | xargs vendor/bin/phpcs -n --standard=rules/phpcs_rules.xml --report=checkstyle --report-file=phpcs_result.xml
     
-    echo "START PROCESS: phpmd"
-    git diff --name-only origin/develop \
-        | grep -e '.php$' \
-        | xargs -I{} vendor/bin/phpmd {} text rules/phpmd_rules.xml \
-        | uniq -c -d 
-        # | uniq -c -d \
-        # | bundle exec checkstyle_filter-git diff origin/develop \
-        # | bundle exec saddler report \
-        # --require saddler/reporter/github \
-        # --reporter Saddler::Reporter::Github::PullRequestReviewComment
-    echo "END PROCESS: phpmd"
+git diff --name-only origin/develop \
+    | grep -e '.php$' \
+    | xargs vendor/bin/phpmd xml rules/phpmd_rules.xml --reportfile phpmd_result.xml
+    # | bundle exec checkstyle_filter-git diff origin/develop \
+    # | bundle exec saddler report \
+    # --require saddler/reporter/github \
+    # --reporter Saddler::Reporter::Github::PullRequestReviewComment
+set -e
+
+echo "********************"
+echo "* save outputs"
+echo "********************"
+LINT_RESULT_DIR="$CIRCLE_ARTIFACTS/lint"
+
+mkdir "$LINT_RESULT_DIR"
+cp -v "phpcs_result.xml" "$LINT_RESULT_DIR/"
+cp -v "phpmd_result.xml" "$LINT_RESULT_DIR/"
+
+echo "********************"
+echo "* select reporter"
+echo "********************"
+if [ -n "$CI_PULL_REQUEST" ]; then
+    # when not pull request
+    REPORTER=Saddler::Reporter::Github::CommitReviewComment
+else
+    REPORTER=Saddler::Reporter::Github::PullRequestReviewComment
 fi
-echo "END SHELL: $0"
+echo "********************"
+echo "* PHP CodeSniffer"
+echo "********************"
+cat phpcs_result.xml \
+    | checkstyle_filter-git diff origin/master \
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
+
+echo "********************"
+echo "* PHP Mess Detector"
+echo "********************"
+cat phpmd_result.xml \
+    | pmd_translate_checkstyle_format translate \
+    | checkstyle_filter-git diff origin/master \
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
+
+echo "********************"
+echo "* end   $0"
+echo "********************"
